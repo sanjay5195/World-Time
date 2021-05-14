b3d575d1cd2c1f37b62e87c236805c9f17fb6620
Sanjay Choudhary <sanjuchoudhary5195@gmail.com>
2021-05-14 16:25:12 +0530

    done routing

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key key}) : super(key: key);
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text("Choose location"),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Text("Choose location"),
+    );
+  }
+}
